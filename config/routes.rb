60175c4dc032110331d24f0f25a62167f9325b6d
岩井　將輝 <>
2020-07-05 14:25:51 +0900

    toppage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'top#index'
-  resources :books
+  devise_for :users
+  resources :users
+  root 'post_image#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
